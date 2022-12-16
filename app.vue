4e5c163d45794e60d3f783a826d553dcb3ba779c
Daniel Roe <daniel@roe.dev>
2022-12-16 11:33:49 +0000

    chore: init

---
+++ app.vue
@@ -0,0 +1,11 @@
+<script setup>
+function raiseEvent() {
+  window.dataLayer.push({ event: 'test', payload: Math.random() })
+}
+</script>
+
+<template>
+  <div>
+    <button @click="raiseEvent">Click me to raise an event</button>
+  </div>
+</template>
